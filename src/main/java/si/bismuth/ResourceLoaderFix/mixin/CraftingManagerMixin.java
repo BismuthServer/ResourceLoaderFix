80383a29b16c9f142456ef538448dec9f8d42256
thdaele <thomas.vandaele2000@gmail.com>
2024-04-29 00:28:22 +0200

    fix ID mismatch affecting recipe book

---
+++ src/main/java/si/bismuth/ResourceLoaderFix/mixin/CraftingManagerMixin.java
@@ -0,0 +1,25 @@
+package si.bismuth.ResourceLoaderFix.mixin;
+
+import net.minecraft.crafting.CraftingManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+@Mixin(CraftingManager.class)
+public class CraftingManagerMixin {
+	@Redirect(
+		method = "load",
+		at = @At(
+			value = "INVOKE",
+			target = "Ljava/util/stream/Stream;iterator()Ljava/util/Iterator;",
+			remap = false
+		)
+	)
+	private static Iterator<Path> fixRecipesIdDependenceOnFileSystemWalkOrder(final Stream<Path> instance) {
+		return instance.sorted((a, b) -> b.toString().compareTo(a.toString())).iterator();
+	}
+}
